73532636106273794cbf5aa9c9119003a0edf7af
luv-mirinae <167957329+luv-mirinae@users.noreply.github.com>
2024-11-18 18:13:26 +0900

    Add templates for starlight

---
+++ starlight/nenerobo/nenerobo.js
@@ -0,0 +1,12 @@
+/**
+ * @file Nenerobo Chatbot Main
+ * @author luv-mirinae
+ * @version 1.0.0-alpha
+ * @license AGPL-3.0
+ */
+
+'use strict';
+
+function onMessage(event) {}
+
+function onStartCompile() {}
